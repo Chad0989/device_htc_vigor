8be2c4801ed93334542d310db93d6e5589775ab7
Chad Froebel <chad0989@rochester.rr.com>
2012-07-30 11:20:04 -0400

    Update product model to correctly represent vigor

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_vigor
 PRODUCT_DEVICE := vigor
-PRODUCT_MODEL := PG86100
+PRODUCT_MODEL := ADR6425LVW
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor BUILD_ID=IML74K BUILD_FINGERPRINT="verizon_wwe/htc_vigor/vigor:4.0.3/IML74K/372320.10:user/release-keys" PRIVATE_BUILD_DESC="3.14.605.10 CL372320 release-keys"
 TARGET_BOOTANIMATION_NAME := vertical-720x1280
