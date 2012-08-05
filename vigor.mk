ca3a6fd53fbaabb2ca56a764df1d81be258ae092
Jonathan Bither <jonbither@gmail.com>
2012-08-05 14:35:33 -0400

    Revert "Forgot that hostapd is already selected when wpa_supplicant_8 is selected" Go back to wpa_supplicant_6 This reverts commit 199f4ac7a7a9b9c8833af33ba495482f75d9dd38.

--- vigor.mk
+++ vigor.mk
@@ -69,6 +69,9 @@ PRODUCT_COPY_FILES += \
 ## Misc
 PRODUCT_PACKAGES += gps.vigor Stk FileManager
 
+# Hostapd
+PRODUCT_PACKAGES += hostapd_cli hostapd
+
 ## CM DSPManager
 PRODUCT_PACKAGES += DSPManager libcyanogen-dsp
 
