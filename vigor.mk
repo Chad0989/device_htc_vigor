9be7e669cf3ed7302f656410c7948c93f911ef9e
Jonathan Bither <jonbither@gmail.com>
2012-08-06 13:02:43 -0400

    Update ramdisk, No longer need HTC init

--- vigor.mk
+++ vigor.mk
@@ -30,7 +30,6 @@ PRODUCT_COPY_FILES += \
 
 ## ramdisk stuffs
 PRODUCT_COPY_FILES += \
-    device/htc/vigor/ramdisk/init:root/init \
     device/htc/vigor/ramdisk/init.vigor.rc:root/init.vigor.rc \
     device/htc/vigor/ramdisk/ueventd.vigor.rc:root/ueventd.vigor.rc \
     device/htc/vigor/ramdisk/init.vigor.usb.rc:root/init.vigor.usb.rc \
