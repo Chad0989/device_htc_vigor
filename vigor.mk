5c56ee2b7293e6eb123305088e99cb7e70617a8e
Chad Froebel <chad0989@rochester.rr.com>
2012-09-21 18:54:48 -0400

    Reduce dalvik heap size
    Change-Id: Ic85af7d794093f48663cad6644fcdd60dec45bae

--- vigor.mk
+++ vigor.mk
@@ -138,5 +138,5 @@ PRODUCT_LOCALES += en_US
 # Inherit configs
 $(call inherit-product-if-exists, vendor/htc/vigor/vigor-vendor.mk)
 $(call inherit-product, device/htc/msm8660-common/msm8660.mk)
-$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product, build/target/product/full_base_telephony.mk)
