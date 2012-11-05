5fb19de765285b55a97a9481ace137cee9e67830
Chad Froebel <chad0989@rochester.rr.com>
2012-11-04 19:21:08 -0500

    Disable BOARD_LEGACY_NL80211_STA_EVENTS as we use an updated bcmdhd driver and firmware.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 BOARD_WLAN_DEVICE_REV            := bcm4330_b2
+BOARD_LEGACY_NL80211_STA_EVENTS := false
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
