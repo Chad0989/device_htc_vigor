cb1e7e0e1a9085b3ba760ad94812aab0cfd0e8d5
Chad Froebel <chad0989@rochester.rr.com>
2013-01-21 11:45:26 -0500

    Add legacy qualcomm GPS HAL and use updated gps.conf
    Change-Id: If9cab476ac3f56ce09f0871316963e67ee5ec971

--- vigor.mk
+++ vigor.mk
@@ -18,7 +18,7 @@
 DEVICE_PACKAGE_OVERLAYS := device/htc/vigor/overlay
 
 # The gps config appropriate for this device
-PRODUCT_COPY_FILES := device/common/gps/gps.conf_US:system/etc/gps.conf
+PRODUCT_COPY_FILES := device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
 
 # Get the sample verizon list of APNs
 PRODUCT_COPY_FILES += device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml
