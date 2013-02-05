1638487cb106ba0e3f8638dc14cf795a44c5a61f
Chad Froebel <chad0989@rochester.rr.com>
2013-02-05 08:06:37 -0500

    Fix build with recent CM merges to display

--- display-legacy/Android.mk
+++ display-legacy/Android.mk
@@ -2,6 +2,7 @@
 #libs to be built for QCOM targets only
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
 
@@ -9,9 +10,8 @@ ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 display-hals += liblight
 endif
 
-endif
-
 #display-hals += libtilerenderer
 
 include $(call all-named-subdir-makefiles,$(display-hals))
-
+endif
+endif
