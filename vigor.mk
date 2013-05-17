d9fcd176c41a9da7fbe80502673b90161a214a97
David Hays <Dhays90@gmail.com>
2013-05-17 18:38:26 -0500

    Disable CDMA/GSM combined APNs

--- vigor.mk
+++ vigor.mk
@@ -21,7 +21,7 @@ DEVICE_PACKAGE_OVERLAYS := device/htc/vigor/overlay
 PRODUCT_COPY_FILES := device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
 
 # CDMA/GSM Combined APNs list
-PRODUCT_COPY_FILES += device/htc/vigor/prebuilt/apns-conf.xml:system/etc/apns-conf.xml
+# PRODUCT_COPY_FILES += device/htc/vigor/prebuilt/apns-conf.xml:system/etc/apns-conf.xml
 
 ## recovery and custom charging
 PRODUCT_COPY_FILES += \
